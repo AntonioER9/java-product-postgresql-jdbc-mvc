4abcfccd42f6be33417e392a6fbd483ecf94dae0
Antonio Espinoza <aespinozareyes9@gmail.com>
2025-08-26 17:19:29 -0400

    Refactor ProductRepositoryServices to integrate ProductDAO for database operations

--- src/main/java/com/antonio/proyecto/product/repository/ProductRepositoryServices.java
+++ src/main/java/com/antonio/proyecto/product/repository/ProductRepositoryServices.java
@@ -1,21 +1,31 @@
 package com.antonio.proyecto.product.repository;
 
+import com.antonio.proyecto.category.model.CategoryDao;
 import com.antonio.proyecto.product.exception.InvalidProductDataException;
 import com.antonio.proyecto.product.exception.ProductNotFoundException;
 import com.antonio.proyecto.product.interfaces.ProductRepository;
 import com.antonio.proyecto.product.model.Product;
 import com.antonio.proyecto.product.model.ProductCategory;
+import com.antonio.proyecto.product.persistence.ProductDAO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class ProductRepositoryServices implements ProductRepository {
-    private final List<Product> products = new ArrayList<>();
+    private final List<Product> products;
+    private final ProductDAO productDAO;
+
+    public ProductRepositoryServices(Connection connection, CategoryDao categoryDao) throws SQLException {
+        productDAO = new ProductDAO(connection, categoryDao);
+        products = productDAO.findAll();
+    }
 
     @Override
     public List<Product> findAll() throws InvalidProductDataException {
-        if(products.isEmpty()){
+        if (products.isEmpty()) {
             throw new InvalidProductDataException("La lista esta vacía");
         }
         return new ArrayList<>(products);
@@ -29,13 +39,15 @@ public class ProductRepositoryServices implements ProductRepository {
     }
 
     @Override
-    public void save(Product product) {
+    public void save(Product product) throws SQLException {
+        productDAO.save(product);
         products.add(product);
     }
 
     @Override
-    public void delete(Long id) {
+    public void delete(Long id) throws SQLException {
         products.removeIf(product -> product.getId().equals(id));
+        productDAO.delete(id);
     }
 
     @Override
@@ -47,11 +59,12 @@ public class ProductRepositoryServices implements ProductRepository {
 
     @Override
     public void update(Optional<Product> existingProductOptional) throws ProductNotFoundException {
-        if(existingProductOptional.isPresent()){
+        if (existingProductOptional.isPresent()) {
             Long idToUpdate = existingProductOptional.get().getId();
             int index = findIndexById(idToUpdate);
             if (index != -1) {
                 products.set(index, existingProductOptional.get());
+                productDAO.update(existingProductOptional.get());
             } else {
                 throw new ProductNotFoundException("El producto que quiere actualizar no existe.");
             }
@@ -65,7 +78,7 @@ public class ProductRepositoryServices implements ProductRepository {
         return products.stream().anyMatch(product -> product.getId().equals(id));
     }
 
-    public int findIndexById(Long id){
+    public int findIndexById(Long id) {
         for (int i = 0; i < products.size(); i++) {
             if (products.get(i).getId().equals(id)) {
                 return i;
